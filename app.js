c2207a69336ac03bb65e9c0cc2026f929cc7b8a7
Andrea Dell'Orco <andrea@adostudio.it>
2017-05-15 21:07:04 +0200

    access token

---
+++ app.js
@@ -0,0 +1,10 @@
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === "EAAYL4J7il9YBAC4Kqz0EunSCvRbPt3WK2n8RdFXZAYjsddnIDVdI4XQs53C99fMW0Mk5Wj37603laz1fsEp5pfoP6hDABSJaLvlUmkOkeL58iPG0LQnngZCSZCuwNmADXZCwSxmXxiclYs5tbY5GpqB4U5x3Cvf9D5vS37zhZCQZDZD") {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
