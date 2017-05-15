63a2473306722624991e75b1c244eadece515557
Andrea Dell'Orco <andrea@adostudio.it>
2017-05-15 21:38:52 +0200

    app js

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
 app.get('/webhook', function(req, res) {
+	
   if (req.query['hub.mode'] === 'subscribe' &&
       req.query['hub.verify_token'] === "EAAYL4J7il9YBAC4Kqz0EunSCvRbPt3WK2n8RdFXZAYjsddnIDVdI4XQs53C99fMW0Mk5Wj37603laz1fsEp5pfoP6hDABSJaLvlUmkOkeL58iPG0LQnngZCSZCuwNmADXZCwSxmXxiclYs5tbY5GpqB4U5x3Cvf9D5vS37zhZCQZDZD") {
     console.log("Validating webhook");
